fc01e021390b18830a8287a4390333d18e0cce4d
Artur Schincariol Rossi <artursrossi4@gmail.com>
2024-11-11 15:23:22 -0300

    add: PSQL Connector + docker-compose.yml file + migrations.sql file + 'obterUsuarios' function

---
+++ src/lib/psql.c
@@ -0,0 +1,16 @@
+PGconn* connection(){
+  // Definir credenciais do banco de dados
+  char *conninfo = "dbname=lingc user=docker password=Teste123!@# host=localhost port=5432";
+
+  // Estabelecer conexão com o banco de dados
+  PGconn *conn = PQconnectdb(conninfo);
+
+  // Verificar o status da conexão, e finalizar o programa em caso de erro de conexão
+  if (PQstatus(conn) != CONNECTION_OK) {
+    printf("Não foi possível se conectar ao banco de dados: %s\n", PQerrorMessage(conn));
+    PQfinish(conn);
+    exit(1);
+  }
+
+  return conn;
+}
