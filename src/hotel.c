0275fbd0c05f5259f1489071c2a4016a8324fe73
MuriloCarvG12 <MuriloCarvalho2583@Outlook.com>
2024-11-11 22:03:48 -0300

    Adiciona o inicio da funcionalidade alugar_quarto Adiciona o struct Quarto e as declarações de funções que o utilizam

---
+++ src/hotel.c
@@ -0,0 +1,73 @@
+#include "Hotel.h"
+#include "stdio.h"
+
+void inicializa_hotel()
+{
+
+};
+
+void alugar_quarto()
+{
+    int numero = 0;
+    int ocupantes = 0;
+    char tipo = '0';
+
+    printf("-- Menu de aluguel para quartos --\n");
+
+
+    /**  Obtendo os dados do usuario **/
+
+    /** Obtendo a classe do quarto **/
+    printf("-- Qual tipo de quarto voce gostaria de alugar? (A,B,C) --\n");
+    while(tipo  == '0')
+    {
+
+        scanf(" %c", &tipo);
+        switch(tipo)
+        {
+            case 'A':
+            {}
+            case 'B':
+            {}
+            case 'C':
+            {}
+            default:
+                printf("informe um tipo valido");
+                tipo  = '0';
+        }
+
+    }
+
+    /** Obtendo o numero do quarto **/
+    printf("-- Qual quarto (1-100) voce gostaria de alugar? --\n");
+        while(numero == 0)
+        {
+        scanf("%d", &numero);
+        if(numero < 0 || numero > 100)
+        {
+            printf("-- Por favor informe um numero valido --\n");
+            numero = 0;
+        }
+        }
+
+    /** Obtendo o numero de ocupantes **/
+    printf("-- Quantos ocupantes? (max 4 pessoas) --\n");
+        while(ocupantes == 0)
+        {
+            scanf("%d", &ocupantes);
+            if(ocupantes < 0 || ocupantes > 4)
+            {
+                printf("-- Por favor informe um numero valido de ocupantes --\n");
+                ocupantes = 0;
+            }
+        }
+
+    /** Obtendo o tipo de quarto **/
+    printf("-- Quantos ocupantes? (max 4 pessoas) --\n");
+
+
+
+
+
+
+}
--- src/hotel.h
+++ src/hotel.h
@@ -1,1 +1,33 @@
 #include "hotel.c"
+#include <stdbool.h>
+
+typedef struct //simula um quarto e suas caracteristicas de interesse gerencial
+{
+    bool quarto_preparado;
+
+    int quarto_numero;
+
+    char numero_ocupantes;
+
+    bool quarto_ocupado; //flag para checar se o quarto está ocupado ou não 0 para falso e 1 para verdadeiro
+
+    float quarto_preco;
+
+    char quarto_classe;
+
+} quarto_hotel;
+
+
+
+typedef quarto_hotel  *p_hotel; // variavel que cria um ponteiro apontando para a estrutura quarto_hotel
+
+extern p_hotel Hotel[100]; //variavel que simula o hotel sendo este tratado como um array contendo 100 quartos
+
+void inicializa_hotel();
+
+void alugar_quarto();
+
+void gerenciar_quarto(); // atualiza informações do quarto
+
+void desalugar_quarto();
+
